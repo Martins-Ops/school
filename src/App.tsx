3f45d33c77973c9aff89bbb318b2864f05ebe141
anon <mykmicheals@gmail.com>
2023-02-25 01:39:53 +0100

    added neccessary routes and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 
 import './App.css';
+import Carousel from './components/Carousel';
 import Header from './components/Header';
+
+import {Routes,Route,Link} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import Login from './screens/Login';
+import ProfileForm from './screens/RegisterStudents';
 
 function App() {
   return (
     <div className="App">
    <Header />
+
+   <Routes>
+
+    <Route  path='/' element={ <HomeScreen /> } /> 
+    <Route  path='/login' element={ <Login /> } /> 
+     <Route  path='/register' element={ <ProfileForm /> } /> 
+
+   </Routes>
+
+ 
+  
     </div>
   );
 }
