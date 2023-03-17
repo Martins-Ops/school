5dbdb785ea77f8c080cacefec5bb210f347fd20e
Micheal <mykmicheals@gmail.com>
2023-03-17 14:53:42 +0100

    Added Aos

--- src/App.tsx
+++ src/App.tsx
@@ -11,21 +11,30 @@ import { Provider } from 'react-redux'
 import store from './store/store';
 import ProfileScreen from './screens/ProfileScreen';
 import Footer from './components/Footer';
+import About from './screens/AboutScreen';
+import ContactScreen from "./screens/ContactScreen";
+import {useEffect} from 'react'
+
+import AOS from "aos";
 
 function App() {
+
+
+  AOS.init()
+
   return (
-   <Provider store={store}>
-   <Header />
-   <Routes>
-    <Route  path='/' element={ <HomeScreen /> } /> 
-    <Route  path='/login' element={ <Login /> } /> 
-     <Route  path='/register' element={ <ProfileForm /> } /> 
-    <Route path='/profile' element={<ProfileScreen />} /> 
-      
+    <Provider store={store}>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<ProfileForm />} />
+        <Route path="/profile" element={<ProfileScreen />} />
+        <Route path="/contact" element={<ContactScreen />} />
       </Routes>
       <Footer />
-
-     </Provider>
+    </Provider>
   );
 }
 
