1792a6c575daaac9c773364bd50ed29b1ee50125
Micheal <mykmicheals@gmail.com>
2023-03-17 21:20:01 +0100

    contact btn

--- src/screens/ContactScreen.tsx
+++ src/screens/ContactScreen.tsx
@@ -70,7 +70,7 @@ function ContactPage() {
             <div>
               <button
                 type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
               >
                 Submit
               </button>
