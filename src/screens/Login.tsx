865d620f120259382733715a08247cb69a25def0
Micheal <mykmicheals@gmail.com>
2023-03-27 12:43:00 +0100

    Changed color btn

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,68 +1,59 @@
-
-import { FaUser, FaLock } from 'react-icons/fa';
-import { post } from '../utils/exports';
-import {useState} from 'react'
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import { FaUser, FaLock } from "react-icons/fa";
+import { post } from "../utils/exports";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function LoginPage() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, []);
+  const [loading, setLoading] = useState(false);
+  const [loginErr, setLoginerr] = useState(false);
+  const [passwordInputError, setPasswordInputErr] = useState(false);
+  const [usernameInputError, setUsernameInputErr] = useState(false);
 
-  const [loading,setLoading] = useState(false)
-   const [loginErr,setLoginerr] = useState(false)
-   const [passwordInputError, setPasswordInputErr] = useState(false)
-   const [usernameInputError, setUsernameInputErr] = useState(false)
+  const navigate = useNavigate();
 
-   const navigate = useNavigate()
+  const [credentials, setCredentials] = useState<{
+    username: string;
+    password: string;
+  }>({ username: "", password: "" });
 
-const [credentials, setCredentials] = useState<{ username: string, password: string }>({ username: '', password: '' });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginerr(false);
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+    setPasswordInputErr(false);
+    setUsernameInputErr(false);
+  };
 
-const handleChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
-  setLoginerr(false)
-  setCredentials({...credentials, [e.target.name]: e.target.value})
-  setPasswordInputErr(false)
-  setUsernameInputErr(false)
-}
- 
-
-const loginHandler=async()=>{
-
- if(credentials.username.length <3){
-    setUsernameInputErr(true)
-  }else if(credentials.password.length <4 ){
-    setPasswordInputErr(true)
-  }
-  
-  else{
-        try{
-    setLoading(true)
-    const response =  await post('login',
-    {
-    username:credentials.username,
-    password:credentials.password
-   })
-   setLoading(false)
-   const data = await response.data
-   console.log(data.token)
-   if (data.status==='error'){
-    setLoginerr(true)
-     setCredentials({...credentials, 'password': ''})
-   }else{
-     localStorage.setItem('token',data.token)
-     localStorage.setItem('isLoggedin','true')
-    navigate('/')
-   }
-
-
-  }catch(error){
-  }
-
-  }
- 
-}
+  const loginHandler = async () => {
+    if (credentials.username.length < 3) {
+      setUsernameInputErr(true);
+    } else if (credentials.password.length < 4) {
+      setPasswordInputErr(true);
+    } else {
+      try {
+        setLoading(true);
+        const response = await post("login", {
+          username: credentials.username,
+          password: credentials.password,
+        });
+        setLoading(false);
+        const data = await response.data;
+        console.log(data.token);
+        if (data.status === "error") {
+          setLoginerr(true);
+          setCredentials({ ...credentials, password: "" });
+        } else {
+          localStorage.setItem("token", data.token);
+          localStorage.setItem("isLoggedin", "true");
+          navigate("/");
+        }
+      } catch (error) {}
+    }
+  };
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8 items-center">
       <div className="bg-white p-10 rounded-lg shadow-lg">
@@ -103,16 +94,17 @@ const loginHandler=async()=>{
             Password must be greater than 4 characters
           </p>
         )}
-        <button
-          onClick={loginHandler}
-          className="w-full bg-blue-900 hover:bg-teal-600 text-white font-bold py-3 rounded-lg mt-6"
-        >
-          {!loading ? "Sign In" : "..."}
-        </button>
+        <Link to="/dashboard">
+          <button
+            onClick={loginHandler}
+            className="w-full bg-gray-700 hover:bg-gray-600 text-white font-bold py-3 rounded-lg mt-6"
+          >
+            {!loading ? "Sign In" : "..."}
+          </button>
+        </Link>
       </div>
     </div>
   );
 }
-
 
 export default LoginPage;
