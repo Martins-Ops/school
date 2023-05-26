5a3466c914b22ff364b38d15590dc5ac57bd123f
Andrew Micheal <mykmicheals@gmail.com>
2023-05-26 21:43:01 +0100

    added loading state

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FaSpinner } from "react-icons/fa";
+
+function LoadingSpinner() {
+  return (
+   <span className="flex items-center justify-center">
+            <FaSpinner className="animate-spin text-white" />
+          </span>
+  );
+}
+
+export default LoadingSpinner;
