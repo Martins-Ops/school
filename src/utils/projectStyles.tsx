84efbd745637c46fd416e4602a563010de27d7ae
Andrew Micheal <mykmicheals@gmail.com>
2023-05-26 21:43:31 +0100

    added new styles

---
+++ src/utils/projectStyles.tsx
@@ -0,0 +1,1 @@
+export const tdStyle = "py-3 text-sm text-gray-500 mb-8 ";
---
+++ src/utils/styles.ts
@@ -0,0 +1,2 @@
+export const inputStyles =
+    "border border-gray-300 py-2 px-3 w-1/2 rounded-lg focus:outline-none focus:border-blue-500";
