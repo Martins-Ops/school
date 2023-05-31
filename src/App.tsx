cac171364e1a86366e7698b4d038b8c2c63215a3
Andrew Micheal <mykmicheals@gmail.com>
2023-05-31 20:42:32 +0100

    removed comments

--- src/App.tsx
+++ src/App.tsx
@@ -22,76 +22,72 @@ function App() {
   // AOS.init();
 
   return (
-    // <Provider store={store}>
-    //   <Header />
-    //   <Routes>
-    //     <Route path="/" element={<HomeScreen />} />
-    //     <Route path="/about" element={<About />} />
-    //     <Route path="/login" element={<Login />} />
-    //     <Route path="/register" element={<ProfileForm />} />
+    <Provider store={store}>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<ProfileForm />} />
 
-    //     {/* <Route
-    //       path="/dashboard"
-    //       element={<ProfileScreen activeLink="dashboard" />}
-    //     /> */}
+        <Route
+          path="/dashboard"
+          element={<ProfileScreen activeLink="dashboard" />}
+        />
 
-    //     <Route path="/dashboard" element={<PrincipalRoute />}>
-    //       <Route
-    //         path="/dashboard"
-    //         element={<ProfileScreen activeLink="dashboard" />}
-    //       />
-    //     </Route>
+        <Route path="/dashboard" element={<PrincipalRoute />}>
+          <Route
+            path="/dashboard"
+            element={<ProfileScreen activeLink="dashboard" />}
+          />
+        </Route>
 
-    //     <Route path="/addstudent" element={<PrincipalRoute />}>
-    //       <Route
-    //         path="/addstudent"
-    //         element={<ProfileScreen activeLink="addstudent" />}
-    //       />
-    //     </Route>
+        <Route path="/addstudent" element={<PrincipalRoute />}>
+          <Route
+            path="/addstudent"
+            element={<ProfileScreen activeLink="addstudent" />}
+          />
+        </Route>
 
-    //        <Route path="/viewteacher" element={<PrincipalRoute />}>
-    //       <Route
-    //         path="/viewteacher"
-    //         element={<ProfileScreen activeLink="viewteacher" />}
-    //       />
-    //     </Route>
+        <Route path="/viewteacher" element={<PrincipalRoute />}>
+          <Route
+            path="/viewteacher"
+            element={<ProfileScreen activeLink="viewteacher" />}
+          />
+        </Route>
 
-    //     <Route path="/addteacher" element={<PrincipalRoute />}>
-    //       <Route
-    //         path="/addteacher"
-    //         element={<ProfileScreen activeLink="addteacher" />}
-    //       />
-    //     </Route>
+        <Route path="/addteacher" element={<PrincipalRoute />}>
+          <Route
+            path="/addteacher"
+            element={<ProfileScreen activeLink="addteacher" />}
+          />
+        </Route>
 
-    //     <Route path="/addsubject" element={<PrincipalRoute />}>
-    //       <Route
-    //         path="/addsubject"
-    //         element={<ProfileScreen activeLink="addsubject" />}
-    //       />
-    //     </Route>
+        <Route path="/addsubject" element={<PrincipalRoute />}>
+          <Route
+            path="/addsubject"
+            element={<ProfileScreen activeLink="addsubject" />}
+          />
+        </Route>
 
-    //     <Route
-    //       path="/mysubjects"
-    //       element={<ProfileScreen activeLink="mysubjects" />}
-    //     />
+        <Route
+          path="/mysubjects"
+          element={<ProfileScreen activeLink="mysubjects" />}
+        />
 
-    //       <Route
-    //       path="/myresults"
-    //       element={<ProfileScreen activeLink="myresults" />}
-    //     />
+        <Route
+          path="/myresults"
+          element={<ProfileScreen activeLink="myresults" />}
+        />
 
-    //     <Route
-    //       path="/mysubjects/:subjectId"
-    //       element={<ProfileScreen activeLink="mysubjects/:subjectId" />}
-    //     />
-    //     <Route path="/contact" element={<ContactScreen />} />
-    //   </Routes>
-    //   <Footer />
-    // </Provider>
-
-    <div className="flex justify-center items-center h-screen">
-      <div className="text-center text-4xl">Hello world</div>
-    </div>
+        <Route
+          path="/mysubjects/:subjectId"
+          element={<ProfileScreen activeLink="mysubjects/:subjectId" />}
+        />
+        <Route path="/contact" element={<ContactScreen />} />
+      </Routes>
+      <Footer />
+    </Provider>
   );
 }
 
