b65659d3fe57f611dffd6b9ee2901d88f9ddc28d
Andrew Micheal <mykmicheals@gmail.com>
2023-06-12 08:01:48 +0100

    added color abstraction to loading spinner

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { FaSpinner } from "react-icons/fa";
 
-function LoadingSpinner() {
+function LoadingSpinner({ color }:any) {
+  var spinnerColor = 'text-'+color
   return (
    <span className="flex items-center justify-center">
-            <FaSpinner className="animate-spin text-white" />
+            <FaSpinner className={`animate-spin ${spinnerColor}`} />
     </span>
   );
 }
