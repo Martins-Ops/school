deb5edee6486783b70a61cc9e21c7698a02c1d4d
amMicheals <mandrew@insight7.io>
2023-08-16 14:31:49 +0100

    added new images

--- src/screens/HomeScreen/ManageMent.tsx
+++ src/screens/HomeScreen/ManageMent.tsx
@@ -3,35 +3,34 @@ import SectionHeader from "../../components/SectionHeader";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import abell from "../../../src/assets/images/abell.png";
 import daniel from "../../../src/assets/images/daniel.png";
-import principal from "../../../src/assets/images/principal.png";
-import vp from "../../../src/assets/images/vp.png";
+import principal from "../../../src/assets/images/principal.jpeg";
+import chaplain from "../../../src/assets/images/chaplain.jpeg";
+
+import vp from "../../../src/assets/images/vp.jpeg";
+
+const imgStyle = "h-[30rem] md:h-[40rem] w-full";
 
 function ManageMent() {
   return (
     <>
       <SectionHeader title="Management" />
       <div className="md:flex md:gap-10 justify-center items-center">
-        <div className=" md:w-1/4 justify-center">
-          {/* <LazyLoadImage src={principal} className="" alt="" /> */}
+        <div className=" md:w-1/3  justify-center">
+          <LazyLoadImage src={chaplain} className={imgStyle} alt="" />
+          <p className="text-center font-bold text-2xl my-8">The Chaplain</p>
+        </div>
+
+        <div className="w-full md:w-1/3 justify-center">
+          <LazyLoadImage src={principal} className={imgStyle} alt="" />
+          <div className="w-full h-full"></div>
           <p className="text-center font-bold text-2xl my-8">The Principal</p>
         </div>
 
-        <div className="w-full md:w-1/4 justify-center">
-          {/* <LazyLoadImage src={vp} className="" alt="" /> */}
-          <div className="w-full h-full"></div>
+        <div className=" md:w-1/3 justify-center">
+          <LazyLoadImage src={vp} className={imgStyle} alt="" />
           <p className="text-center font-bold text-2xl my-8">
-            The Vice Principal
+            The Vice principal
           </p>
-        </div>
-
-        <div className=" md:w-1/4 justify-center">
-          {/* <LazyLoadImage src={abell} className="" alt="" /> */}
-          <p className="text-center font-bold text-2xl my-8">The Accountant</p>
-        </div>
-
-        <div className=" md:w-1/4 justify-center">
-          {/* <LazyLoadImage src={daniel} className="" alt="" /> */}
-          <p className="text-center font-bold text-2xl my-8">The Manager</p>
         </div>
       </div>
     </>
